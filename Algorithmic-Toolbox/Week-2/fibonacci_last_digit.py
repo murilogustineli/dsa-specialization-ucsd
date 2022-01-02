7db3529089efa260a65d21d2500f5e53391a300b
murilogustineli <52141042+murilogustineli@users.noreply.github.com>
2022-01-02 16:53:06 -0500

    Add files via upload

---
+++ Algorithmic-Toolbox/Week-2/fibonacci_last_digit.py
@@ -0,0 +1,34 @@
+# Uses python3
+import sys
+
+def get_fibonacci_last_digit_naive(n):
+    if n <= 1:
+        return n
+
+    previous = 0
+    current  = 1
+
+    for _ in range(n - 1):
+        previous, current = current, previous + current
+
+    return current % 10
+
+def last_digit_of_fibonacci_number(n):
+    assert 0 <= n <= 10 ** 7
+    if n <= 1:
+        return n
+    a, b = 0, 1                 # Assigning values to variables a and b
+    for i in range(n-1):
+        add = (a + b) % 10      # Sum a and b
+        a, b = b, add           # Update values of a, b, and add
+    return add
+
+
+# if __name__ == '__main__':
+#     input = sys.stdin.read()
+#     n = int(input)
+#     print(get_fibonacci_last_digit_naive(n))
+
+if __name__ == '__main__':
+    input_n = int(input())
+    print(last_digit_of_fibonacci_number(input_n))
