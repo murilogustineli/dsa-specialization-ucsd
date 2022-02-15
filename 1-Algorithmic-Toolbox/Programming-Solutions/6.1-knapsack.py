4e98e51f4c87b063596918fe301657695fc2ef5e
murilogustineli <52141042+murilogustineli@users.noreply.github.com>
2022-02-15 09:42:55 -0500

    Update 6.1-knapsack.py

--- 1-Algorithmic-Toolbox/Programming-Solutions/6.1-knapsack.py
+++ 1-Algorithmic-Toolbox/Programming-Solutions/6.1-knapsack.py
@@ -4,20 +4,6 @@ from sys import stdin
 import numpy as np
 
 
-def optimal_weight(W, w):
-    # write your code here
-    result = 0
-    for x in w:
-        if result + x <= W:
-            result = result + x
-    return result
-
-# if __name__ == '__main__':
-#     input = sys.stdin.read()
-#     W, n, *w = list(map(int, input.split()))
-#     print(optimal_weight(W, w))
-
-
 # Maximum Amount of Gold
 def maximum_gold(capacity, weights):
     assert 1 <= capacity <= 10 ** 4
