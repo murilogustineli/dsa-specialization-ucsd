9fc976cf5171a09ee628ce8d5dc735f65d3eb9a1
murilogustineli <52141042+murilogustineli@users.noreply.github.com>
2022-02-20 21:05:15 -0500

    Update 1.1-check_brackets.py

--- 2-Data-Structures/Programming-Solutions/1.1-check_brackets.py
+++ 2-Data-Structures/Programming-Solutions/1.1-check_brackets.py
@@ -5,51 +5,44 @@ from collections import namedtuple
 Bracket = namedtuple("Bracket", ["char", "position"])
 
 
+# Function to check corresponding opening and closing brackets
 def are_matching(left, right):
     return (left + right) in ["()", "[]", "{}"]
 
-
+  
+# Function to find mismatch brackets
 def find_mismatch(text):
     opening_brackets_stack = []
+    
+    # Iterate over string text
     for i, next in enumerate(text):
         if next in "([{":
-            # Process opening bracket, write your code here
-            pass
+            # Process opening bracket
+            opening_brackets_stack.append(Bracket(next,i))
 
         if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
-
-
-def checkBracket(string):
-    dic = {'(': ')', '[': ']', '{': '}'}
-    stack = []
-
-    if len(string) <= 1:
-        return 1
-
-    for i, char in enumerate(string):
-        if char in dic.keys():
-            stack.append(char)
-        elif char not in dic.values():
-            continue
-        else:
-            if len(stack) < 1:
-                return string.index(char)+1
-            top = stack.pop()
-            if dic[top] != char:
-                return i+1
-    if len(stack) == 0:
-        return 'Success'
+            # Process closing bracket
+            if not opening_brackets_stack:
+                return Bracket(next, i)
+            # Check for corresponding opening and closing brackets
+            top = opening_brackets_stack.pop()
+            if not are_matching(top.char, next):
+                return Bracket(next, i)
+    # Return None if all brackets are corresponding
+    if not opening_brackets_stack:
+        return None
     else:
-        return string.index(top) + 1
+        return opening_brackets_stack[0]
 
 
 def main():
     text = input()
-    mismatch = checkBracket(text)
+    mismatch = find_mismatch(text)
     # Printing answer, write your code here
-    print(mismatch)
+    if mismatch == None:
+        print("Success")
+    else:
+        print(mismatch.position+1)
 
 
 if __name__ == "__main__":
